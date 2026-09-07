761d4b67acb66396db4d413402fa025018480724
Allan Rein <allanrodrigo18@gmail.com>
2026-09-07 17:29:11 -0300

    fix: configura payload_format_version 2.0 e rota ANY /{proxy+} no api gateway

--- main.tf
+++ main.tf
@@ -164,34 +164,48 @@ resource "aws_lambda_function" "api_lambda" {
 # 5. API GATEWAY (COM CORS HABILITADO)
 # ------------------------------------------------------------------------------
 
+# 1. Configuração do API Gateway com CORS Global
 resource "aws_apigatewayv2_api" "http_api" {
   name          = "api_sistema_tecnicos"
   protocol_type = "HTTP"
 
   cors_configuration {
     allow_origins = ["*"]
-    allow_methods = ["GET", "POST", "OPTIONS", "PUT", "DELETE"]
-    allow_headers = ["content-type", "authorization", "x-amz-date", "x-api-key"]
+    allow_methods = ["*"]
+    allow_headers = ["*"]
     max_age       = 300
   }
 }
 
-resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id           = aws_apigatewayv2_api.http_api.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = aws_lambda_function.api_lambda.invoke_arn
-}
-
-resource "aws_apigatewayv2_route" "api_route" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "POST /diagnostico"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
-
+# 2. Estágio Padrão
 resource "aws_apigatewayv2_stage" "api_stage" {
   api_id      = aws_apigatewayv2_api.http_api.id
   name        = "$default"
   auto_deploy = true
+}
+
+# 3. Integração com a Lambda definindo explicitamente payload_format_version = "2.0"
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.api_lambda.invoke_arn
+  payload_format_version = "2.0"
+}
+
+# 4. Rota Curinga para aceitar QUALQUER método e caminho (POST /login, GET /linhas, etc.)
+resource "aws_apigatewayv2_route" "api_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+# 5. Permissão de Invocação pelo API Gateway
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
 
 resource "aws_lambda_permission" "api_mw" {
