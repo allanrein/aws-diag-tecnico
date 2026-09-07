deb1b615f5b411455d8f53d72efd7c7b742e84a8
Allan Rein <allanrodrigo18@gmail.com>
2026-09-07 16:32:04 -0300

    ci: configura backend s3 para sincronizar tfstate com github actions

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "allan-tfstate-sistema-tecnicos-2026"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
 provider "aws" {
   region = "us-east-1"
 }
